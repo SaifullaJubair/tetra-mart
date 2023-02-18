2bf1d6471ecd6d60dc131b0a36f8c578dde516ba
SaifullaJubair <saifullajubair6@gmail.com>
2023-02-18 16:05:44 +0600

    name add

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<h2>Hello</h2>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h2>Hello</h2>
+        <h2>Saifulla</h2>
+      </header>
+    </div>
+  );
 }
 
 export default App;
